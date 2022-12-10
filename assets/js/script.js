a3ed649246f9e4c3daf74d1d554dec756ae7ebd8
Mirna Samaan <mirna.nabih92@gmail.com>
2022-12-10 12:22:24 +0200

    plan items multiselect

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,30 @@ $(function () {
     $("#profileImage").click(function (e) {
         $("#imageUpload").click();
     });
+
+    $(".plan-items li > div").on('click', function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $(this).find("input").prop("checked", false);
+        }
+        else {
+            $(this).addClass('active');
+            $(this).find("input").prop("checked", true);
+        }
+    })
+
 })
+
+function sConsole(event) {
+    event.preventDefault();
+    var arts = document.getElementById("arts");
+    var children = document.getElementById("children");
+    var food = document.getElementById("food");
+    var market = document.getElementById("market");
+    var sports = document.getElementById("sports");
+    console.log("arts checked", arts.checked);
+    console.log("children checked", children.checked);
+    console.log("food checked", food.checked);
+    console.log("market checked", market.checked);
+    console.log("sports checked", sports.checked);
+}
